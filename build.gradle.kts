44640c5b753250819db0ce363a03e2907601d810
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2022-10-01 13:04:33 +0200

    Remove dependency on kotlin plugin.serialization as we try to use Jackson

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,6 @@ val mockkVersion = "1.13.2"
 plugins {
     application
     kotlin("jvm") version "1.7.10"
-    kotlin("plugin.serialization") version "1.7.20"
     id("io.gitlab.arturbosch.detekt") version "1.21.0"
     id("ca.cutterslade.analyze") version "1.9.0"
 }
